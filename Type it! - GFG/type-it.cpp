a1e9e1c45883094c11f30565545ef363e69877e2
Prashant Kumar <110713100+prashantk81@users.noreply.github.com>
2023-01-26 12:23:57 +0530

    Added solution - LeetHub

--- Type it! - GFG/type-it.cpp
+++ Type it! - GFG/type-it.cpp
@@ -10,19 +10,23 @@ using namespace std;
 class Solution {
   public:
     int minOperation(string s) {
-        if(s.size() <= 3)
-            return s.size();
-        int ans = INT_MAX;
-        string str = "";
-        for(int i=0; i<s.size(); i++){
-            str = str + s[i];
-            string temp = s.substr(i+1, str.size());
-            if(temp == str){
-                int ctr = str.size() + 1 + (s.size() - str.size()*2);
-                ans = min(ans, ctr);
+        // code here
+        int size=s.size();
+        int half=size/2;
+        int red=0;
+        while(half>=1)
+        {
+            string str1=s.substr(0,half);
+            string str2=s.substr(half,half);
+            // cout<<str1<<" "<<str2<<endl;
+            if(str1==str2)
+            {
+                red=half-1;
+                break;
             }
+            half-=1;
         }
-        return (ans==INT_MAX)? s.size(): ans;
+        return size-red;
     }
 };
 
